8db5984553c69026ba2e5547c22e167fd41f500f
iamminji <sberryyogurt@gmail.com>
2018-01-06 17:48:06 +0900

    테스트

--- algorizum/runner.py
+++ algorizum/runner.py
@@ -1,18 +1,26 @@
 # -*- coding: utf8 -*-
 
+from datetime import datetime
+
 from crawler.crawler import AlgorizumCrawler
 from crawler.rules import AlgorizumRules
 
 
 def test():
+    algospot_user = ["홍시"]
     algospot_rules = {'class': {AlgorizumRules.recent_published_date: 'submitted_on[1]'}}
 
     algospot_crawler = AlgorizumCrawler(parsing_rules=algospot_rules)
     algospot_crawler.url = 'https://algospot.com/judge/submission/recent/?user=(USER_ID)'
-    algospot_crawler.user = '홍시'
+    algospot_crawler.user = algospot_user
 
     result = algospot_crawler.crawl()
-    print result['홍시']
+    today = datetime.today()
+
+    for user in algospot_user:
+        user_recent_published_date = result[user][AlgorizumRules.recent_published_date]
+        if today != user_recent_published_date:
+            print "no result", user, 'recent published', user_recent_published_date.strftime("%Y년 %m월 %d일")
 
 
 if __name__ == "__main__":
